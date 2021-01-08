1e066afb0cc841f2fcddd8dcb532b036161ab726
Bill Ni <bill.8ni@gmail.com>
2021-01-08 13:57:05 -0500

    Create indices.py

---
+++ backend/indices.py
@@ -0,0 +1,23 @@
+# Terrain
+class Dirt:
+  normal = 0
+  undiscovered = 58
+
+class Stone:
+  normal = 1
+  undiscovered = 59
+
+class Water:
+  normal = 2
+  undiscovered = 60
+
+class Farm:
+  normal = 3
+
+class Grass:
+  normal = 4
+  undiscovered = 62
+
+class Tree:
+  normal = 5
+  undiscovered = 63
